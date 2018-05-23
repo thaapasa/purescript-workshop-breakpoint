65c5aedc8ad23c529e1ed551e14eec3a6e301b0c
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2018-05-23 10:23:14 +0300

    Do exercise 2

--- exercise2/test/Exercise2.purs
+++ exercise2/test/Exercise2.purs
@@ -3,6 +3,7 @@ module Test.Exercise2 where
 import Prelude
 
 import Control.Monad.Eff (Eff)
+import Data.Array (foldl, head)
 import Data.Array as Array
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
@@ -29,11 +30,11 @@ tests = do
         let shape = Line (Point {x: 0.0, y: 0.0}) (Point {x: 1.0, y: 1.0})
         Assert.equal 0.0 (area shape)
         
-    testSkip "2 Newtype: get value out of newtype" do
+    test "2 Newtype: get value out of newtype" do
       let adminId = AdminId "superuser247"
       Assert.equal "superuser247" (unwrapId adminId)
       
-    suiteSkip "3 Maybe: printing usernames" do
+    suite "3 Maybe: printing usernames" do
       test "Prints username if given a username" do
         let maybeUsername = Just "johndoe"
         Assert.equal "Username: johndoe" (showUsername maybeUsername)
@@ -41,7 +42,7 @@ tests = do
         let maybeUsername = Nothing
         Assert.equal "No username" (showUsername maybeUsername)
       
-    suiteSkip "4 Either: either the thing we want or an error" do
+    suite "4 Either: either the thing we want or an error" do
       test "Prints username if given a user" do
         let userResult = Right { id: 1, username: "johndoe" }
         Assert.equal "johndoe" (showUserResult userResult)
@@ -49,7 +50,7 @@ tests = do
         let userResult = Left "Could not find user"
         Assert.equal "Could not find user" (showUserResult userResult)
         
-    suiteSkip "5 Maybe: Find largest number" do
+    suite "5 Maybe: Find largest number" do
       test "returns largest number" do
         Assert.equal
           (Just 176)
@@ -77,22 +78,30 @@ pi :: Number
 pi = 3.14
       
 area :: Shape -> Number
+area (Circle pt rad) = pi * rad * rad
+area (Rectangle pt width height) = width * height
 area _ = 0.0
 
 newtype AdminId = AdminId String
 
 unwrapId :: AdminId -> String
-unwrapId _ = "fixme"
+unwrapId (AdminId id) = id
 
 largestNumber :: Array Int -> Maybe Int
-largestNumber _ = Nothing
+largestNumber [] = Nothing
+largestNumber [a] = Just a
+largestNumber x = case (head x) of
+  Just a -> Just $ foldl (\i j -> max i j) a x
+  _ -> Nothing
 
 showUsername :: Maybe String -> String
-showUsername _ = "fixme"
+showUsername (Just u) = "Username: " <> u
+showUsername _ = "No username"
 
 type User =
   { id :: Int
   , username :: String }
 
 showUserResult :: Either String User -> String
-showUserResult _ = "fixme"
+showUserResult (Right u) = u.username
+showUserResult _ = "Could not find user"
